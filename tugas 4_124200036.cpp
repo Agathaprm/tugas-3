4477d9c320c2ecb20174c43a61d681505ca2ef51
Agathaprm <primaagatha68@gmail.com>
2020-11-07 10:28:16 +0700

    tugas 3

---
+++ tugas 4_124200036.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+main ()
+{
+		int N, M, pilih, kali, pangkat, i;
+	cout<<" PERKALIAN DAN PERPANGKATAN "<<endl;
+	cout<<"----------------------------"<<endl;
+	cout<<"1. Perkalian "<<endl;
+	cout<<"2. Perpangkatan "<<endl;
+	cout<<"\n\n";
+	cout<<"----------------------------"<<endl;
+	cout<<"Pilih: "; cin>> pilih;
+	switch (pilih)
+	{	
+	case 1 :
+		
+		cout<< "Masukan Nilai N = ";
+		cin >> N;
+		cout<< "Masukan Nilai M = ";
+		cin >> M;
+		cout<<"\n\n";
+		cout <<"Hasil Kali: " << " " << N << " ^ " << M << endl;
+			kali = 0;
+			for (int i = 1; i <= M; i++)
+			{
+				kali += N;
+					if (i != M)
+					{cout << N << " + ";}
+					else
+					{cout << N << " = "<<kali;}		
+			}
+	break;
+	case 2 :
+		cout<< "Masukan Nilai N = ";
+		cin >> M;
+		cout<< "Masukan Nilai M = ";
+		cin >> N;
+		cout<<"\n\n";
+			cout <<"Hasil Pangkat: " << " " << N << " ^ " << M << endl;
+			pangkat = 1;
+			for (int i = 1; i <= M; i++)
+			{
+				pangkat *= N;
+					if (i != M)
+					{cout << N << " X ";}
+					else
+					{cout << setprecision(999999) << N<< " = "<<pangkat;}		
+			}
+			
+	break;
+	default :
+		cout << ("INPUT SALAH!\n");
+		
+	}	
+		
+}
